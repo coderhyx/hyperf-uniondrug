b067a62c0334e8953128ced3c75607affa04906e
黄亚雄 <huangyaxiong@uniondrug.com>
2021-04-13 19:59:35 +0800

    init

---
+++ src/ClientFactory.php
@@ -0,0 +1,36 @@
+<?php
+namespace Uniondrug\HyperfUniondrug;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use Hyperf\Guzzle\CoroutineHandler;
+use Hyperf\Utils\Coroutine;
+use Psr\Container\ContainerInterface;
+
+class ClientFactory extends \Hyperf\Guzzle\ClientFactory
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function create(array $options = []): GuzzleClient
+    {
+        $stack = null;
+        if (Coroutine::inCoroutine()) {
+            $stack = HandlerStack::create(new CoroutineHandler());
+        }
+        $config = array_replace(['handler' => $stack], $options);
+        if (method_exists($this->container, 'make')) {
+            return $this->container->make(GuzzleClient::class, ['config' => $config]);
+        }
+        return new GuzzleClient($config);
+    }
+    
+}
---
+++ src/TraceLogger.php
@@ -0,0 +1,76 @@
+<?php
+namespace Uniondrug\HyperfUniondrug;
+use Hyperf\HttpServer\Request;
+use Hyperf\Redis\Redis;
+use Hyperf\Utils\Context;
+use Hyperf\Utils\Coroutine;
+use Psr\Log\LoggerInterface;
+class TraceLogger
+{
+    const VERSION = 10.0;
+    /**
+     * @Inject()
+     * @var Redis
+     */
+    private  $redis;
+
+    /**
+     * @Inject()
+     * @var Request
+     */
+    private $request;
+
+    public $traceId;
+
+    /**
+     * @Value("config.app_name")
+     */
+    private $appName;
+
+    /**
+     * @var TraceLogger;
+     */
+    protected $logger;
+
+    
+    public function info($data, $action='', $method = 'GET')
+    {
+
+        $keyRandom = 't' . date('dHis') . mt_rand(1001, 9999) . mt_rand(1001, 9999);
+        // 2. parser logger
+        $list = [];
+        $list[$keyRandom . sprintf("%04d", 0)] = [
+            'time'          => date('Y-m-t H:i:s.sss', time()),
+            'level'         => 'INFO',
+            'action'        => $action,
+            'module'        => $this->appName,
+            'duration'      => 0,
+            'pid'           => 0,
+            'requestId'     => Context::get('traceId'),
+            'requestMethod' => $method,
+            'requestUrl'    => $this->request->url(),
+            'traceId'       => Context::get('traceId'),
+            'spanId'        => Context::get('spanId'),
+            'parentSpandId' => '',
+            'serverAddr'    => '',
+            'taskId'        => Coroutine::id(),
+            'taskName'      => '',
+            'version'       => self::VERSION,
+            'content'       => $data
+        ];
+        $keys = [];
+        foreach ($list as $key => $val) {
+            $this->redis->set('logger:' . $key, json_encode($val, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), 86400);
+            $keys[] = 'logger:' . $key;
+        }
+        $this->redis->rPush('logger:list', ... $keys);
+        $res = is_array($data) ? json_encode($data, true) : $data;
+        $this->logger->info(trim($res));
+    }
+
+    public function debug($data)
+    {
+        $data = is_array($data) ? json_encode($data) : $data;
+        echo $data.PHP_EOL;
+    }
+}
